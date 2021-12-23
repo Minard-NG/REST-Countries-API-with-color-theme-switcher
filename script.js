c12d50e488dfb1f98adf26498f2c83bb12996b73
Minard-NG <michael4sure20@gmail.com>
2021-12-23 17:05:38 +0100

    successfully completed the passing of page data logic between the index and info page

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ if (typeof (Storage !== undefined)) {
     if (!localStorage.theme) {
         localStorage.theme = 'default';
     }
+    if (!localStorage.name) {
+        localStorage.theme = 'peru';
+    }
 } else {
     console.log("Browser does not support client-side storage to keep track of scores");
 }
@@ -61,11 +64,11 @@ filterBtn.addEventListener('click', () => {
 
 //handling asynchronous calss
 async function getData(url) {
+    console.log(url);
     const response = await fetch(url);
 
     let data = await response.json();
-
-    console.log(data)
+    console.log(data);
     renderGrid(data)
 
 }
@@ -75,12 +78,12 @@ function renderGrid(data) {
     content = '';
 
     data.forEach(obj => {
-        content += `<a class="grid__item" href="./info.html">
+        content += `<a class="grid__item" href="./info.html?name=${obj.name.common.toLowerCase()}">
         <div class="grid__item-img">
           <img src=${obj.flags.png} alt="country flag" />
         </div>
         <div class="grid__item-content">
-          <h3>${obj.name.common}</h3>
+          <h3 id='countryName'>${obj.name.common}</h3>
           <p>Population: <span>${obj.population}</span></p>
           <p>Region: <span>${obj.region}</span></p>
           <p>Capital: <span>${obj.capital}</span></p>
@@ -91,7 +94,10 @@ function renderGrid(data) {
 
     mainGrid.innerHTML = content;
 }
-getData('https://restcountries.com/v3.1/all'); //load the data on page load;
+if(document.location.pathname==='/index.html'){
+    getData('https://restcountries.com/v3.1/all'); //load the data on page load;
+}
+
 
 
 //search event handler
@@ -121,4 +127,67 @@ function filterer(evt) {
     console.log(getData(`https://restcountries.com/v3.1/region/${region}`))
 
 
+}
+
+//event handler for country cards
+async function getInfoData(url) {
+    const response = await fetch(url);
+
+    let data = await response.json();
+    
+    renderInfoData(data)
+}
+
+function renderInfoData(data){
+   let mainInfoArea = document.querySelector('.infomain');
+   let content = '';
+
+   data.forEach((obj)=>{
+    content+=`<div class="flag">
+    <img src=${obj.flags.png} alt="country flag" />
+  </div>
+
+  <div class="info">
+    <h3 >${obj.name}</h3>
+    <div class="info__1">
+      <p>Native Name: <span>${obj.nativeName}</span></p>
+      <p>Population: <span>${obj.population}</span></p>
+      <p>Region: <span>${obj.region}</span></p>
+      <p>Sub Region: <span>${obj.subregion}</span></p>
+      <p>Capital: <span>${obj.capital}</span></p>
+    </div>
+
+    <div class="info__2">
+      <p>Top Level Domain: <span>${obj.topLevelDomain[0]}</span></p>
+      <p>Currencies: <span>${obj.currencies[0].name}</span></p>
+      <p>Languages: <span>${obj.languages[0].name}</span></p>
+    </div>
+
+    <div class="info__footer">
+      <p>Border Countries:</p>
+      <div class="info__footer-links">
+        <a href="#" class="button">France</a>
+        <a href="#" class="button">France</a>
+        <a href="#" class="button">France</a>
+        <a href="#" class="button">France</a>
+        <a href="#" class="button">France</a>
+      </div>
+    </div>
+  </div>`
+   });
+
+   mainInfoArea.innerHTML =content;
+}
+
+if(document.location.pathname==='/info.html'){
+    let url = document.location.href;
+
+    let tmp = url.split('?')[1];
+
+    let name = tmp.split('=')[1];
+   
+    console.log(name);
+    console.log(`https://restcountries.com/v2/name/${name}`)
+
+    getInfoData(`https://restcountries.com/v2/name/${name}`); //load the data on page load;
 }
