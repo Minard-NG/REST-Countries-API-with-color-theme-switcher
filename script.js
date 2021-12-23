eb78e1a55db7bed0903b523c8e05e77d44a05a9e
Minard-NG <michael4sure20@gmail.com>
2021-12-23 21:26:16 +0100

    added spinner for better user experience

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ if (typeof (Storage !== undefined)) {
     if (!localStorage.theme) {
         localStorage.theme = 'default';
     }
-    if (!localStorage.name) {
-        localStorage.theme = 'peru';
-    }
 } else {
     console.log("Browser does not support client-side storage to keep track of scores");
 }
@@ -94,7 +91,7 @@ function renderGrid(data) {
 
     mainGrid.innerHTML = content;
 }
-if(document.location.pathname==='/index.html'){
+if (document.location.pathname === '/index.html') {
     getData('https://restcountries.com/v3.1/all'); //load the data on page load;
 }
 
@@ -134,60 +131,117 @@ async function getInfoData(url) {
     const response = await fetch(url);
 
     let data = await response.json();
-    
+    console.log(data)
     renderInfoData(data)
+   
 }
 
-function renderInfoData(data){
-   let mainInfoArea = document.querySelector('.infomain');
-   let content = '';
+function renderInfoData(data) {
+    let mainInfoArea = document.querySelector('.infomain');
+    let content = '';
 
-   data.forEach((obj)=>{
-    content+=`<div class="flag">
-    <img src=${obj.flags.png} alt="country flag" />
-  </div>
-
-  <div class="info">
-    <h3 >${obj.name}</h3>
-    <div class="info__1">
-      <p>Native Name: <span>${obj.nativeName}</span></p>
-      <p>Population: <span>${obj.population}</span></p>
-      <p>Region: <span>${obj.region}</span></p>
-      <p>Sub Region: <span>${obj.subregion}</span></p>
-      <p>Capital: <span>${obj.capital}</span></p>
-    </div>
-
-    <div class="info__2">
-      <p>Top Level Domain: <span>${obj.topLevelDomain[0]}</span></p>
-      <p>Currencies: <span>${obj.currencies[0].name}</span></p>
-      <p>Languages: <span>${obj.languages[0].name}</span></p>
-    </div>
-
-    <div class="info__footer">
-      <p>Border Countries:</p>
-      <div class="info__footer-links">
-        <a href="#" class="button">France</a>
-        <a href="#" class="button">France</a>
-        <a href="#" class="button">France</a>
-        <a href="#" class="button">France</a>
-        <a href="#" class="button">France</a>
+    if(Array.isArray(data)){
+        data.forEach((obj) => {
+            content += `<div class="flag">
+        <img src=${obj.flags.png} alt="country flag" />
       </div>
-    </div>
-  </div>`
-   });
+    
+      <div class="info">
+        <h3 >${obj.name}</h3>
+        <div class="info__1">
+          <p>Native Name: <span>${obj.nativeName}</span></p>
+          <p>Population: <span>${obj.population}</span></p>
+          <p>Region: <span>${obj.region}</span></p>
+          <p>Sub Region: <span>${obj.subregion}</span></p>
+          <p>Capital: <span>${obj.capital}</span></p>
+        </div>
+    
+        <div class="info__2">
+          <p>Top Level Domain: <span>${obj.topLevelDomain[0]}</span></p>
+          <p>Currencies: <span>${obj.currencies[0].name}</span></p>
+          <p>Languages: <span>${obj.languages[0].name}</span></p>
+        </div>
+    
+        <div class="info__footer">
+          <p>Border Countries:</p>
+          <div class="info__footer-links">
+           
+          </div>
+        </div>
+      </div>`;
+            mainInfoArea.innerHTML = content;
+    
+            let border_container = mainInfoArea.querySelector('.info__footer-links');
+            content = '';
+            if (obj.borders){
+                obj.borders.forEach((border) => {
+                    content += `<a href="./info.html?code=${border}" class="button">${border}</a>`
+                });
+                border_container.innerHTML= content;
+            }
+    
+        });
+    }else{
+    
+            content += `<div class="flag">
+        <img src=${data.flags.png} alt="country flag" />
+      </div>
+    
+      <div class="info">
+        <h3 >${data.name}</h3>
+        <div class="info__1">
+          <p>Native Name: <span>${data.nativeName}</span></p>
+          <p>Population: <span>${data.population}</span></p>
+          <p>Region: <span>${data.region}</span></p>
+          <p>Sub Region: <span>${data.subregion}</span></p>
+          <p>Capital: <span>${data.capital}</span></p>
+        </div>
+    
+        <div class="info__2">
+          <p>Top Level Domain: <span>${data.topLevelDomain[0]}</span></p>
+          <p>Currencies: <span>${data.currencies[0].name}</span></p>
+          <p>Languages: <span>${data.languages[0].name}</span></p>
+        </div>
+    
+        <div class="info__footer">
+          <p>Border Countries:</p>
+          <div class="info__footer-links">
+            
+          </div>
+        </div>
+      </div>`;
+            mainInfoArea.innerHTML = content;
+    
+            let border_container = mainInfoArea.querySelector('.info__footer-links');
+            content = '';
+            if (data.borders){
+                data.borders.forEach((border) => {
+                    content += `<a href="./info.html?code=${border}" class="button">${border}</a>`
+                });
+                border_container.innerHTML= content;
+            }
+            
+    
+            
+    
+        }
+    }
+    
 
-   mainInfoArea.innerHTML =content;
-}
-
-if(document.location.pathname==='/info.html'){
+if (document.location.pathname === '/info.html') {
     let url = document.location.href;
 
-    let tmp = url.split('?')[1];
+    let tmp = url.split('?')[1],
+        name, code;
 
-    let name = tmp.split('=')[1];
-   
-    console.log(name);
-    console.log(`https://restcountries.com/v2/name/${name}`)
-
-    getInfoData(`https://restcountries.com/v2/name/${name}`); //load the data on page load;
+    if(tmp.split('=')[0] ==='name'){
+        name = tmp.split('=')[1];
+        getInfoData(`https://restcountries.com/v2/name/${name}`); //load the data on page load;
+    }
+    if(tmp.split('=')[0] ==='code'){
+        code = tmp.split('=')[1].toLowerCase();
+        console.log(code);
+        console.log(getInfoData(`https://restcountries.com/v2/alpha/${code}`))
+        getInfoData(`https://restcountries.com/v2/alpha/${code}`); //load the data on page load;
+    }  
 }
